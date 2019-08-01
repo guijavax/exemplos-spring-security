aa2edadcfa2ba543a86345e01915752c90d248dc
Guilherme Carneiro Borges <gcarneib@everis.com>
2019-08-01 15:54:12 -0300

    exemplo-spring-security

---
+++ exmplo-basic-security/src/main/java/com/example/api/exmplobasicsecurity/ExmploBasicSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.api.exmplobasicsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExmploBasicSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExmploBasicSecurityApplication.class, args);
+	}
+
+}
